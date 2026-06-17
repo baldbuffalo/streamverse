f810e3fa9cb2143e6a079bcdd942e3f594ce6c03
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-06-17 17:17:25 +0400

    Add OkHttp support for Coil image loading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
 
     // Image loading
     implementation("io.coil-kt.coil3:coil-compose:3.5.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.5.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
