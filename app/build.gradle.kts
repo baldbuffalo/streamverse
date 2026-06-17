a2c46c8729ca9e3c0d096603ebe90b799deec9fc
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-06-17 15:41:10 +0400

    chore: bump compileOptions to Java 26 (Gradle 9.5.1 officially supports JavaVersion.VERSION_26)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_26
+        targetCompatibility = JavaVersion.VERSION_26
     }
 
     buildFeatures {
